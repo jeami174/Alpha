63ecfff4185bcd49204479a4f24cb08bf6c2ac2c
Jeanette Mikkelsen <jeanette.mikkelsen@utb.ecutbildning.se>
2025-03-15 09:58:38 +0100

    Worked on the client side. The work has been started.

---
+++ WebApp/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[Route("Admin")]
+public class AdminController : Controller
+{
+    public IActionResult Members()
+    {
+        return View();
+    }
+
+    [Route("Clients")]
+    public IActionResult Clients()
+    {
+        return View();
+    }
+}
