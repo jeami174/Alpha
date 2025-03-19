dbb281759c12a35bd0bf46ff23833d7bb528c1ef
Jeanette Mikkelsen <jeanette.mikkelsen@utb.ecutbildning.se>
2025-03-19 20:23:48 +0100

    I have completed the design of the Sign-Up form. The fields are validated in the same way as the other fields on the page. However, I am considering implementing custom validation for both Sign-In and Sign-Up, as users might need more detailed feedback. Third-party authentication still needs to be added.

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -1,19 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
 
     public class AuthController : Controller
     {
+
+        [HttpGet]
         public IActionResult SignIn()
         {
             return View();
         }
 
-        [Route("signup")]
+        [HttpPost]
+        public IActionResult SignIn(string test)
+        {
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult SignUp()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult SignUp(SignUpFormModel formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState
+                    .Where(x => x.Value?.Errors.Count > 0)
+                    .ToDictionary(
+                        kvp => kvp.Key,
+                        kvp => kvp.Value?.Errors.Select(x => x.ErrorMessage).ToArray()
+                    );
+
+                return BadRequest(new { success = false, errors });
+            }
+
+            //var result = await _clientService.AddClientAsync(form);
+            //if (result)
+            //{
+            //    return Ok(new { success = true });
+            //}
+            //else
+            //{
+            // return Problem("unable to submit data")
+            //}
+
+            return Ok(new { success = true }); //radera senare
         }
 
         public new IActionResult SignOut()
---
+++ WebApp/Models/SignUpFormModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class SignUpFormModel
+    {
+        [Display(Name = "Full Name", Prompt = "Your full name")]
+        [Required(ErrorMessage = "Required")]
+        public string FullName { get; set; } = null!;
+
+        [Display(Name = "Email", Prompt = "Your email address")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Required")]
+        [RegularExpression(@"^[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$", ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Password", Prompt = "Enter your password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Required")]
+        public string Password { get; set; } = null!;
+
+        [Display(Name = "Confirm Password", Prompt = "Confirm your password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = null!;
+
+        [Display(Name = "Accept Terms")]
+        [Required(ErrorMessage = "You must accept the terms and conditions")]
+        public bool AcceptTerms { get; set; }
+    }
+}
